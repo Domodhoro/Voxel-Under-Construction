f961c05ce5e9ba790043b461b08ab015b322e80d
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2023-01-08 13:33:13 -0300

    Update luaScript.hpp

--- src/luaScript.hpp
+++ src/luaScript.hpp
@@ -17,21 +17,21 @@ public:
         lua_close(m_L);
     }
 
-    void table(const char* name) {
+    void table(const char *name) {
         lua_getglobal(m_L, name);
 
         if (!lua_istable(m_L, -1)) {
-            throw std::string {"Falha ao ler arquivo lua."};
+            throw std::string {"Falha ao ler tabela."};
         }
     }
 
     template<typename T>
-    T load(const char* key) {
+    T load(const char *key) {
         lua_pushstring(m_L, key);
         lua_gettable(m_L, -2);
 
         if (!lua_isnumber(m_L, -1)) {
-            throw std::string {"Falha ao ler arquivo lua."};
+            throw std::string {"Falha ao ler dado da tabela."};
         }
 
         T result {lua_tonumber(m_L, -1)};
