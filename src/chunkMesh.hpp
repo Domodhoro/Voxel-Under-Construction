8e6b416011b1df5bfc749419f2d0f4888c397bdc
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2023-01-15 01:03:45 -0300

    Update chunkMesh.hpp

--- src/chunkMesh.hpp
+++ src/chunkMesh.hpp
@@ -5,28 +5,28 @@ struct Vertex {
     float X {0.0f}, Y {0.0f}, Z {0.0f}, U {0.0f}, V {0.0f}, T {0.0f};
 };
 
-unsigned int Mesh(std::vector<Vertex> &Vertice, int x, int y, int z, bool &F, bool &B, bool &R, bool &L, bool &U, bool &D, int texture) {
+unsigned int Mesh(std::vector<Vertex> &Vertice, int x, int y, int z, bool &F, bool &B, bool &R, bool &L, bool &U, bool &D, int id) {
     const auto X {static_cast<float>(x)}, Y {static_cast<float>(y)}, Z {static_cast<float>(z)};
 
     struct {
         float F {0.0f}, B {0.0f}, R {0.0f}, L {0.0f}, U {0.0f}, D {0.0f};
     } Texture;
 
-    if (texture == 1.0f) {
+    if (id == 1.0f) {
         Texture.F = 2.0f;
         Texture.B = 2.0f;
         Texture.R = 2.0f;
         Texture.L = 2.0f;
         Texture.U = 1.0f;
         Texture.D = 3.0f;
-    } else if (texture == 2.0f) {
+    } else if (id == 2.0f) {
         Texture.F = 3.0f;
         Texture.B = 3.0f;
         Texture.R = 3.0f;
         Texture.L = 3.0f;
         Texture.U = 3.0f;
         Texture.D = 3.0f;
-    } else if (texture == 3.0f) {
+    } else if (id == 3.0f) {
         Texture.F = 4.0f;
         Texture.B = 4.0f;
         Texture.R = 4.0f;
